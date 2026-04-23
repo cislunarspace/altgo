b160290252de4efd37a7028cc41397c123ee88a7
ouyangjiahong <ouyangjiahong@github.com>
2026-04-23 12:14:33 +0800

    feat: add unified version constants for packaging scripts

---
+++ packaging/scripts/versions.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Unified version constants for all packaging scripts.
+# Source this file: source "$(dirname "${BASH_SOURCE[0]}")/versions.sh"
+
+WHISPER_CPP_VERSION="1.8.4"
+FFMPEG_VERSION="7.1.1"
